e70a4b2ad4613436825bca02249e796ae0193af3
HaoWang95 <alanwang0028@gmail.com>
2021-12-21 13:58:23 +1100

    update

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,17 @@
+import { useLoaderData, Link } from 'remix';
+import type {Post} from "../../post";
+import { loader } from "../../post";
+
+export default function Posts() {
+  const posts = useLoaderData<Post[]>();
+  return (
+    <div>
+      <h1>Posts</h1>
+      <ul>
+          {posts.map(post => (
+              <li>{post}</li>
+          ))}
+      </ul>
+    </div>
+  );
+}
